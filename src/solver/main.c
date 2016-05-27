28250d003c1f0398e7c793fca7241dc6b0901fb0
favazquez <favazquez>
2016-05-27 00:33:20 -0400

    node creation

--- src/solver/main.c
+++ src/solver/main.c
@@ -11,15 +11,48 @@ bool debugging=true;
 uint8_t width=0;
 uint8_t height=0;
 uint8_t limit=0;
+uint8_t active_col_count=0;
+uint8_t active_row_count=0;
 
 bool* active_rows;
 bool* active_cols;
 uint8_t** goal;
 
+Node* init_node()
+{
+  Node* r = malloc(sizeof(Node));
+
+  active_rows = malloc(sizeof(bool) * height);
+
+  for(uint8_t row = 0; row < height; row++)
+  {
+    active_rows[row] = false;
+  }
+
+  active_cols = malloc(sizeof(bool) * width);
+
+  for(uint8_t col = 0; col < height; col++)
+  {
+    active_cols[col] = false;
+  }
+
+  r->state = malloc(sizeof(uint8_t*) * height);
+  for(uint8_t row = 0; row < height; row++)
+  {
+    r->state[row] = malloc(sizeof(uint8_t) * width);
+  }
+
+  goal = malloc(sizeof(uint8_t*) * height);
+  for(uint8_t row = 0; row < height; row++)
+  {
+    goal[row] = malloc(sizeof(uint8_t) * width);
+  }
+
+  return r;
+}
 
 Node* read_input(char* filename)
 {
-   Node* r = malloc(sizeof(Node));
    FILE* file = fopen(filename, "r");
 
    char buf[256];
@@ -28,26 +61,18 @@ Node* read_input(char* filename)
    fscanf(file,"%s", buf);
    fscanf(file,"%hhu", &width);
 
-
-   active_rows = malloc(sizeof(bool) * height);
-   for(uint8_t row = 0; row < height; row++)
-   {
-     active_rows[row] = false;
-   }
-   active_cols = malloc(sizeof(bool) * width);
-   for(uint8_t col = 0; col < height; col++)
-   {
-     active_cols[col] = false;
-   }
+   Node* r = init_node();
 
    uint8_t actives;
    uint8_t active;
+
    fscanf(file,"%hhu", &actives);
    for(uint8_t col = 0; col < actives; col++)
    {
      fscanf(file,"%hhu", &active);
      active_cols[active] = true;
    }
+
    fscanf(file,"%hhu", &actives);
    for(uint8_t row = 0; row < actives; row++)
    {
@@ -55,10 +80,8 @@ Node* read_input(char* filename)
      active_rows[active] = true;
    }
 
-   r->state = malloc(sizeof(uint8_t*) * height);
    for(uint8_t row = 0; row < height; row++)
    {
-     r->state[row] = malloc(sizeof(uint8_t) * width);
      for(uint8_t col = 0; col < width; col++)
      {
        fscanf(file,"%hhu", &r->state[row][col]);
@@ -68,10 +91,8 @@ Node* read_input(char* filename)
    fscanf(file,"%s", buf);
    fscanf(file,"%hhu", &limit);
 
-   goal = malloc(sizeof(uint8_t*) * height);
    for(uint8_t row = 0; row < height; row++)
    {
-     goal[row] = malloc(sizeof(uint8_t) * width);
      for(uint8_t col = 0; col < width; col++)
      {
        fscanf(file,"%hhu", &goal[row][col]);
@@ -136,9 +157,32 @@ void print_global_variables()
   printf("-------------------------------------\n");
 
 }
+int count_children(Node* n)
+{
+  return 0;
+}
+void gen_children(Node* n)
+{
+  n->child_count = count_children(n);
 
+  for(uint8_t row = 0; row < height; row++)
+  {
+    //shift right
+
+    //shift left
+  }
+
+  for(uint8_t col = 0; col < height; col++)
+  {
+    //active_cols[col] = false;
+  }
+
+
+}
 int main(int argc, char *argv[])
 {
+
+  watcher_open(argv[1]);
 
   Node* root= read_input(argv[1]);
 
@@ -148,9 +192,12 @@ int main(int argc, char *argv[])
     print_node(root);
   }
 
-  watcher_open(argv[1]);
 
-  //TODO SOLVE 
+  //TODO SOLVE
+  while (2>1)
+  {
+    /* code */
+  }
 
   watcher_close();
 
